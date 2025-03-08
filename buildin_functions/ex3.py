5c6ebc18baf8c731209ac93a3613d78aa6c7e1b7
OlzhasDuisenbay <o_duisenbay@kbtu.kz>
2025-03-08 23:46:59 +0500

    Delete buildin_functions/ex3.py

--- buildin_functions/ex3.py
+++
@@ -1,6 +0,0 @@
-"""Write a Python program with builtin function that returns True
-if all elements of the tuple are true."""
-def my_func(t):
-    return all(t)
-t = (True,True,True)
-print(my_func(t))
